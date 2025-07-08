81643373485ca4f18f9876ff87183c99e923853e
Rafael Simionato <rafaasimi@hotmail.com>
2025-07-08 17:04:57 -0300

    feat: configuração inicial do front-end

---
+++ web/src/pages/room.tsx
@@ -0,0 +1,20 @@
+import { Link, useParams, Navigate } from "react-router-dom";
+
+type RoomParams = {
+  roomId: string;
+};
+
+export function Room() {
+  const params = useParams<RoomParams>();
+
+  if (!params.roomId) {
+    return <Navigate replace to="/" />;
+  }
+
+  return (
+    <div>
+      <h1>Sala 1: {params.roomId}</h1>
+      <Link to="/">Ir para sala</Link>
+    </div>
+  );
+}
